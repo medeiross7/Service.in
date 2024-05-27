8ff03196aaba1f544080cca326a9fb228b3088f3
medeiross7 <125074668+medeiross7@users.noreply.github.com>
2024-05-27 20:40:12 -0300

    Add files via upload

---
+++ Service.in/sobre.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="css.css">
+    <link rel="stylesheet" type="text/css" href="public/css.css" />
+</head>
+<body>
+    <div style="height: 20vh; background-color: #238E68; display: flex;  flex-direction: column;" class="incial">
+        <div style="margin-left:   35vw ; heigth: 20vh;">
+            <img style="heigth: 10vh;" src="img/Servece 1.png" alt="">
+        </div>
+        <div style="display: flex; margin-top: 30px; justify-content: space-around;">
+            <div></div>
+            <div> <a class="btnhead" href="" style="color: white; font-size: 18px; text-decoration: none;">Cadastre-se</a> </div>
+            <div> <a class="btnhead" href="" style="color: Black; font-size: 18px; text-decoration: none;">Prestar Serviços</a></div>
+            <div> <a class="btnhead" href="" style="color: Black; font-size: 18px; text-decoration: none;">Solicitar Serviço</a></div>
+            <div> <a class="btnhead" href="sobre.php" style="color: Black; font-size: 18px; text-decoration: none;">Sobre</a></div>
+            <div><a style="color: black; text-decoration:none; display: flex; align-items: center;" href=""> PT/BR <img style="width: 40px;" src="img/istockphoto-530810572-612x612.jpg" alt=""></a></div>
+            <div><label class="switch">
+                    <input type="checkbox">
+                    <span class="slider"></span>
+                </label></div>
+        </div>
+    </div>
+    <div class="divimg">
+        
+    </div>
+</body>
+</html>
