06b45f44ecc93434e3c4388f522f15bcf54a2601
medeiross7 <125074668+medeiross7@users.noreply.github.com>
2024-09-17 16:41:18 -0300

    Sprint 08

--- conexao.php
+++ conexao.php
@@ -1,15 +1,25 @@
 <?php
 
-$usuario = 'root';
-$senha = '';
-$database = 'cadastro';
-$host = 'localhost';
-
-$mysqli = new mysqli($host, $usuario, $senha, $database);
+$host = 'localhost'; // Endereço do servidor de banco de dados
+$dbname = 'cadastro'; // Nome do banco de dados
+$user = 'root'; // Nome de usuário do banco de dados
+$pass = ''; // Senha do banco de dados
+$mysqli = new mysqli($host, $user, $pass, $dbname);
 if($mysqli->error){
     die("falha ao conhectar o banco de dados". $mysqli->error);
 }
 else{
     echo "Conexao efetuada com sucesso";
+}
+
+try {
+    // Cria uma instância de PDO para a conexão com o banco de dados
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname", $user, $pass);
+    // Define o modo de erro do PDO para exceções
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+} catch (PDOException $e) {
+    // Se houver um erro na conexão, exibe a mensagem de erro
+    echo 'Conexão falhou: ' . $e->getMessage();
+    exit();
 }
 ?>
--- perfil_colaborador.php
+++ perfil_colaborador.php
@@ -1,16 +1,26 @@
 <?php
-    
-    include_once('conexao.php');
-    
-    session_start();
-   // print_r($_SESSION);
-    if((!isset($_SESSION['email'])== true) and (!isset($_SESSION['senha']) == true)){
-        header('Location: login.php');
-    }
-    $logado = $_SESSION['email']; //email conectado na sessao, atentar na hora de fazer o layout
+include_once('conexao.php');
 
+session_start();
+if ((!isset($_SESSION['email']) == true) and (!isset($_SESSION['senha']) == true)) {
+    header('Location: login.php');
+    exit();
+}
 
-    
+$email = $_SESSION['email'];
+
+// Prepare a consulta SQL para obter o nome do usuário
+$sql = "SELECT nome FROM usuarios WHERE email = ?";
+$stmt = $pdo->prepare($sql);
+$stmt->execute([$email]);
+
+// Verifique se o usuário foi encontrado e obtenha os dados
+$user = $stmt->fetch(PDO::FETCH_ASSOC);
+if ($user) {
+    $nome = htmlspecialchars($user['nome']);
+} else {
+    $nome = 'Nome não encontrado';
+}
 ?>
 
 <!DOCTYPE html>
@@ -20,109 +30,97 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
     <style>
-          body{
+        body {
             background-color: #238E68;
             font-family: Arial, Helvetica, sans-serif;
-            font-size: 3vh
-            
-        }    
-
-        .div-cin{
-            background-color: gray;
-            height: auto ;
-             width: 75% ;
-             padding: 7.5vh 10vh 7.5vh 20vh;
-            margin-top: 7.5vh;
-              
+            font-size: 3vh;
         }
-
-        .d-mae{
+        .div-cin {
+            background-color: gray;
+            height: auto;
+            width: 75%;
+            padding: 7.5vh 10vh 7.5vh 20vh;
+            margin-top: 7.5vh;
+        }
+        .d-mae {
             display: grid;
-
             place-items: center;
             align-items: center;
         }
-
-        .mg{
-             margin-bottom: 3vh;
-             margin-left: -10vh;
-             display:flex;
-             
+        .mg {
+            margin-bottom: 3vh;
+            margin-left: -10vh;
+            display: flex;
         }
-
-        p{
+        p {
             margin-left: 20vh;
         }
-
-        .inp{
+        .inp {
             margin-left: 5vh;
-            width: 25vh ;
-            height: 2vh ;
-            padding: 0.5vh 2vh 0.5vh 2vh;
+            width: 25vh;
+            height: 2vh;
+            padding: 0.5vh 2vh;
         }
-
-        .sel{
+        .sel {
             margin-left: 5vh;
-            width: 25vh ;
-            height: 3vh ;
-   
+            width: 25vh;
+            height: 3vh;
         }
-        .aperfil{
+        .aperfil {
             text-decoration: none;
-             color: black;
+            color: black;
         }
-        
-        .aperfil:hover{
+        .aperfil:hover {
             background-color: rgba(0, 0, 0, 0.5);
         }
-
-</style>
+    </style>
 </head>
 <body>
-
-
-        <form action="delete.php" method="POST">
+    <form action="delete.php" method="POST">
         <div class="d-mae">
+            <div class="div-cin">
+                <div class="mg"><?php echo $nome; ?></div>
+            </div>
 
             <div class="div-cin">
-    <div class="mg"> <?php echo htmlspecialchars($nome); ?></div>
-    <div class="mg">Sobrenome</div>               
-    </div>
+                <div class="mg">
+                    Alterar telefone <p></p> (**)*****-**** <input class="inp" type="tel">
+                </div>
+                <div class="mg">
+                    Alterar email <p></p> *************@gmail.com <input class="inp" type="text">
+                </div>
+                <div class="mg">
+                    Adicionar tipo de serviço
+                    <div>
+                        <select class="sel" name="cars">
+                            <option value="volvo">Hidraulica</option>
+                            <option value="saab">Eletrica</option>
+                            <option value="fiat">Mecanico</option>
+                            <option value="audi">Gogo boy</option>
+                        </select>
+                    </div>
+                </div>
+            </div>
 
+            <div class="div-cin">
+                <a class="aperfil" href="alterarsobre.php">
+                    <div class="mg">Alterar sobre <p></p></div>
+                </a>
+                <a class="aperfil" href="alterarsenha.php">
+                    <div class="mg">Alterar senha<p></p></div>
+                </a>
+                <a class="aperfil" href="alterarfoto.php">
+                    <div class="mg">Alterar Fotos do Perfil <p></p></div>
+                </a>
+            </div>
 
-    <div class="div-cin">
-    <div class="mg">Alterar telefone <p></p>  (**)*****-**** <input class="inp" type="tel"> </div>
-    <div class="mg">Alterar email <p></p> *************@gmail.com <input class="inp" type="text"> </div>
-    
-
-    <div class="mg">Adicionar tipo de serviço 
-        <div>  <select class="sel" name="cars">
-              <option value="volvo">Hidraulica</option>
-                <option value="saab">Eletrica</option>
-                  <option value="fiat">Mecanico</option>
-                     <option value="audi">Gogo boy</option>
-                        </select> </div>
-      </div>
-
+            <div>
+                <a href="index.php">
+                    <input class="btn-del" type="button" value="Excluir Conta" name="deletar">
+                </a>
+                <input class="btn-del" type="button" value="Alterar" name="alterar">
+            </div>
         </div>
-
-
-        <div class="div-cin">
-        <a class="aperfil"  href="alterarsobre.php"><div class="mg">Alterar sobre <p></p> </div></a>
-        <a class="aperfil"  href="alterarsenha.php"><div class="mg">Alterar senha<p></p></div></a>
-        <a class="aperfil"  href="alterarfoto.php"><div class="mg">Alterar Fotos do Perfil <p></p></div></a>
-        </div>
-
-        <div>
-            <a href="index.php"> <input class="btn-del"   type="button" value="Excluir Conta" name="deletar"></a>
-       
-        <input class="btn-del"  type="button" value="Alterar" name="alterar">
-        </div>
-
-
-        </div>
-        </form>
-       
-    
+    </form>
 </body>
 </html>
